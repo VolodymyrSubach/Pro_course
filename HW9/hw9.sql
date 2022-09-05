f32a92ebd7bd2f0d5af0dfcf2f7f47023f763bed
VolodymyrSubach <110986725+VolodymyrSubach@users.noreply.github.com>
2022-09-05 13:10:52 +0300

    Update hw9.sql

--- HW9/hw9.sql
+++ HW9/hw9.sql
@@ -1,5 +1,5 @@
-SELECT a.Title'Album name', g.Name'Genre', t.Composer'Artist or Group', ROUND((SUM(t.Milliseconds)/60000))'Duration(Minutes)',
-(SUM(t.Bytes))/1048576'Size(MB)', SUM(t.UnitPrice)'Album Price', COUNT(t.Name)'Count Tracks'
+SELECT a.Title'Album name', g.Name'Genre', t.Composer'Artist or Group', ROUND(SUM(t.Milliseconds)/60000.0)'Duration(Minutes)',
+ROUND((SUM(t.Bytes))/1048576.0, 2)', SUM(t.UnitPrice)'Album Price', COUNT(t.Name)'Count Tracks'
 FROM albums a
 	JOIN tracks t on a.AlbumId = t.AlbumId
 	JOIN genres g on t.GenreId = g.GenreId
